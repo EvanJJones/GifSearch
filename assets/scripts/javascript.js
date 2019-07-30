6e2ba08a924203d7e73df07da4caeb74c11e3c24
Evan Jones <evanjones@fastmail.net>
2019-07-30 13:40:09 -0400

    everything working

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,7 +1,7 @@
 function getGifs(searchTerm) {
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=S8V0rc7PKunaLSNh6MkcXe3qnxaveF0F&limit=5";
-
+    $("#gallery").html("")
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=S8V0rc7PKunaLSNh6MkcXe3qnxaveF0F&q=" + searchTerm + "&limit=5&offset=0&rating=G&lang=en";
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -11,11 +11,27 @@ function getGifs(searchTerm) {
         } else {
             //this is what will get executed if it works
             displayGifs(response);
+
         }
-    });
+    })
 }
 //path to image
 //  data[0].images.original.url
 function displayGifs(response) {
+    for (var i = 0; i < 5; i++) {
+        var imgUrl = response.data[i].images.original.url;
+        console.log(imgUrl);
+        $("#gallery").append("<img src='" + imgUrl + "'>")
+    }
+}
 
+
+console.log("test")
+$("#search").on("click", searchGifs);
+
+
+function searchGifs() {
+    var searchTerm = $("#searchTerm").val();
+    getGifs(searchTerm);
+    console.log(searchTerm);
 }
